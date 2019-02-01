c000dc8ee41822857e2c5c9e86bff70476c0e425
Mbah Dinan <46408979+mbahdinan@users.noreply.github.com>
2019-02-01 08:09:12 +0700

    Add files via upload

---
+++ MD-disqus-hack-2.js
@@ -0,0 +1,1 @@
+﻿!function(){var e=document.createElement(&quot;script&quot;);e.type=&quot;text/javascript&quot;,e.async=!0,e.src=&quot;//&quot;+disqus_shortname+&quot;.disqus.com/blogger_item.js&quot;,(document.getElementsByTagName(&quot;head&quot;)[0]||document.getElementsByTagName(&quot;body&quot;)[0]).appendChild(e)}();
