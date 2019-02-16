a1426f89da271f38879a533726b814bb65fcb5bf
Mbah Dinan <46408979+mbahdinan@users.noreply.github.com>
2019-02-16 12:23:39 +0700

    Delete defer-adsense.js

--- defer-adsense.js
+++
@@ -1,2 +0,0 @@
-﻿// Lazy Load AdSense
-var lazyadsense=!1;window.addEventListener("scroll",function(){(0!=document.documentElement.scrollTop&&!1===lazyadsense||0!=document.body.scrollTop&&!1===lazyadsense)&&(!function(){var e=document.createElement("script");e.type="text/javascript",e.async=!0,e.src="https://cdn.ampproject.org/v0/amp-ad-0.1.js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(e,a)}(),lazyadsense=!0)},!0);
